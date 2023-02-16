e20ad82034c8b1845d1c0ef26e67176f3f12df2e
Tracy Eke <eke.tracy@gmail.com>
2023-02-16 10:29:58 -0800

    7. 100-intel edit2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function which contains a
+ *
+ * c program that prints a line to the standard error
+ *
+ * Return: return 1 (success)
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
