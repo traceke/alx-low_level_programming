f67c272c13cafc72f03b3b4d0384277394e7ab94
Tracy Eke <eke.tracy@gmail.com>
2023-02-19 12:01:36 -0800

    8. Print base 16.c edit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,9 @@ int main(void)
 	int i;
 	char j;
 
-	for (i = 0 ; i >= 9 ; i++)
+	for (i = 0 ; i <= 9 ; i++)
 		putchar(i + '0');
-	for (j = 'a' ; j >= 'f' ; j++)
+	for (j = 'a' ; j <= 'f' ; j++)
 		putchar(j);
 	return (0);
 }
