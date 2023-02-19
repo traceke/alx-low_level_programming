86207861e0a3c32fc77e6791c54578f9b7f383bb
Tracy Eke <eke.tracy@gmail.com>
2023-02-19 12:14:31 -0800

    9. Print comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: always returns 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+		putchar(i + '0');
+	putchar(',');
+	putchar(' ');
+	return (0);
+}
