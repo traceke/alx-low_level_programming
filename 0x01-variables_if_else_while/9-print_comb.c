dc10bc440f258f2b3e2730d1efdfa326a4be504d
Tracy Eke <eke.tracy@gmail.com>
2023-02-19 12:24:29 -0800

    9. Print comb.c edit 2

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,9 +10,14 @@ int main(void)
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
-		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		if (i == 9)
+			putchar(i +'0');
+		else
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	return (0);
 }
