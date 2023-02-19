a4b8bb6c8e6defce61bd8473a9982225aab7ef57
Tracy Eke <eke.tracy@gmail.com>
2023-02-19 11:43:58 -0800

    7. Print tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: always returns 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i <= 'a'; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
