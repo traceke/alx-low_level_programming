95a776a8003d06e5bf48a9cfc6fabfb41c966082
Tracy Eke <eke.tracy@gmail.com>
2023-02-19 11:59:36 -0800

    8. Print base 16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: always returns 0
+ */
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i >= 9 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j >= 'f' ; j++)
+		putchar(j);
+	return (0);
+}
