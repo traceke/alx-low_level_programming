0ab5f38b524b08da5e0e56ce62bb157343d8294a
Tracy Eke <eke.tracy@gmail.com>
2023-02-22 10:31:33 -0800

    0-main.c fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,12 +5,11 @@
 * 
 * Return: 0
 */
-
-int main(void)
+void positive_or_negative(int i);
 {
         int i;
 
-        i = -145;
+        i = 98;
         positive_or_negative(i);
 
         return (0);
