2b287cfa5a8cc9bc4f76178b32739b514f2c374e
Tracy Eke <eke.tracy@gmail.com>
2023-02-22 15:53:38 -0800

    debugging task 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
-int _putchar(char c);
+int _putchar(char c);
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
