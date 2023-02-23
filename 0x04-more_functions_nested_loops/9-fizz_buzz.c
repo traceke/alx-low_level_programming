0b5c4a4c9de084df31fd2bd99e56119c54da736f
Tracy Eke <eke.tracy@gmail.com>
2023-02-23 15:59:38 -0800

    9. fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,25 +15,24 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
-		if (i == 100 && i % 5 == 0)
+		if (i != 100)
 		{
-			printf("Fizz");
+			printf(" ");
 		}
 	}
-	printf("\n");
 	return (0);
 }
