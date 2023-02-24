515aa42d4710f350484beb6ef3f373c878a3aa83
Tracy Eke <eke.tracy@gmail.com>
2023-02-24 06:21:09 -0800

    101-print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number - prints only integers
+ *
+ * @n: the integer to be printed
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar(45);
+		n = -n;
+	}
+	if (n > 0 && (n / 10) >= 1)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
+
