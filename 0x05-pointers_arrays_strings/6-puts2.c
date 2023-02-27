a478d46e21f985e2aece890eea9d805a40d94c48
Tracy Eke <eke.tracy@gmail.com>
2023-02-27 10:37:10 -0800

    6. puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character.
+ *
+ * @str: the string to be printed
+ */
+void puts2(char *str)
+{
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+		j++;
+	for (i = 0 ; i < j ; i = (i + 2))
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
