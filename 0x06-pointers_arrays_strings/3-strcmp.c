b8f1f52cece41fcde8ec81e1671619774028e040
Tracy Eke <eke.tracy@gmail.com>
2023-03-02 10:20:50 -0800

    3. strcmp fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,14 +12,17 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	for (i = 0 ; s1[i] != '\0' || s2[i] != '\0' ; i++)
 	{
-		if (s1[1] != s2[1])
+		if (s1[i] != s2[i])
 		{
-			return (s1[1] - s2[1]);
+			if (s1[i] < s2[i])
+				return (s1[i] - s2[i]);
+			else if (s1[i] > s2[i])
+				return (s1[i] - s2[i]);
 		}
-		i++;
+		else
+			return (0);
 	}
 	return (0);
 }
