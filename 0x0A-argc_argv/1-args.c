99e965e425057c691a0d42b1deb9daf987a75284
Tracy Eke <eke.tracy@gmail.com>
2023-03-11 09:53:09 -0800

    1. args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed into it
+ *
+ * @argc: argument count
+ * @argv: argument vector(array of strings)
+ *
+ * Return: returns 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
